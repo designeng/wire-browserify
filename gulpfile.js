f9ea55cc16623d9493bf2e51a1db398e65aa9358
designeng <denissavenok@gmail.com>
2015-12-01 19:44:41 +0300

    .babelrc with presets added

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var watchify = require('watchify');
 var babel = require('babelify');
 
 function compile(watch) {
-  var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
+  var bundler = watchify(browserify('./src/index.js', { debug: true, extensions: ['.js', '.json'] }).transform(babel));
 
   function rebundle() {
     bundler.bundle()
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+"use strict"
+
 import one from 'one';
 let test = 123;
