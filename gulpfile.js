7098dd3d347f5f06f78eea599e89001e2624337c
designeng <denissavenok@gmail.com>
2015-12-01 20:15:16 +0300

    express server; chalk

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
+var chalk = require('chalk');
 var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var babel = require('babelify');
+var express = require('express');
+var lrserver = require('tiny-lr')();
+var refresh = require('gulp-livereload');
+var livereload = require('connect-livereload');
+
+var livereloadport = 35729;
+var serverport = 5000;
+
+var server = express();
+server.use(livereload({
+  port: livereloadport
+}));
+server.use(express.static('./build'));
 
 function compile(watch) {
   var bundler = watchify(browserify('./src/index.js', { debug: true, extensions: ['.js', '.json'] }).transform(babel));
@@ -19,10 +33,15 @@ function compile(watch) {
       .pipe(gulp.dest('./build'));
   }
 
+  function reload() {
+    bundler.bundle().pipe(refresh(lrserver));
+  }
+
   if (watch) {
     bundler.on('update', function() {
       console.log('-> bundling...');
       rebundle();
+      reload();
     });
   }
 
@@ -33,7 +52,13 @@ function watch() {
   return compile(true);
 };
 
+gulp.task('serve', function() {
+  server.listen(serverport);
+  lrserver.listen(livereloadport);
+  console.log(chalk.blue('-> server listening on ' + serverport + ' port'));
+});
+
 gulp.task('build', function() { return compile(); });
 gulp.task('watch', function() { return watch(); });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve', 'watch']);
